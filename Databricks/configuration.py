49e547a029fa36e424ed0e15f08898f3e25e62fa
buirang <103221542+buirang@users.noreply.github.com>
2022-11-29 21:16:03 -0800

    Add files via upload

---
+++ Databricks/configuration.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+dataPipelinePath= f"/dbfs/FileStore/movies"
+rawPath = dataPipelinePath + "raw/"
+bronzePath = dataPipelinePath + "bronze/"
+silverPath = dataPipelinePath + "silver/"
+
+# COMMAND ----------
+
+
