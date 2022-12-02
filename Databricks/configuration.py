fdb80a402dea62635cef2b93a08b15c47b667f7d
buirang <103221542+buirang@users.noreply.github.com>
2022-12-01 19:14:01 -0800

    Delete Databricks directory

--- Databricks/configuration.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-dataPipelinePath= f"/dbfs/FileStore/movies"
-rawPath = dataPipelinePath + "raw/"
-bronzePath = dataPipelinePath + "bronze/"
-silverPath = dataPipelinePath + "silver/"
-
-# COMMAND ----------
-
-
